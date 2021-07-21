79022e3058bdfaedd95509f65ffb356fe3a90d28
luisalejandrovm <LuisAlejandroVM@hotmail.com>
2021-07-21 16:54:24 -0500

    all project

---
+++ examples/get_channel_info.py
@@ -0,0 +1,11 @@
+'''
+    Description:
+    Get information from Youtube in a simple way.
+
+    Author: AlejandroV
+    Version: 0.1.0
+'''
+import AVMYT as yt
+
+channel = yt.getChannelInfo("luisito") # get channel
+print(channel["name"] + " tiene " + channel["subs"] + " y un total de " + channel["videos"]) # prints info of Luisito Comunica
---
+++ examples/open_song.py
@@ -0,0 +1,11 @@
+'''
+    Description:
+    Get information from Youtube in a simple way.
+
+    Author: AlejandroV
+    Version: 0.1.0
+'''
+import AVMYT as yt
+
+# opens the song in Youtube
+yt.play("bad guy")
