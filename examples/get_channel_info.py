7764e64282db45e81a9d8e8280155fd3e0aac1a9
Alejandro V <luisalejandrovm@hotmail.com>
2023-03-16 01:45:21 -0600

    Username

--- examples/get_channel_info.py
+++ examples/get_channel_info.py
@@ -1,8 +1,6 @@
 '''
-    Description:
-    Get information from Youtube in a simple way.
-
-    Author: AlejandroV
+    Description: Get information from Youtube in a simple way.
+    Author: aulerjbailey
     Version: 0.1.0
 '''
 import AVMYT as yt
